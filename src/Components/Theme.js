05074ec03ecbb899587cd8af2d5c4e80da8960c5
Karemmk <100991187+Karemmk@users.noreply.github.com>
2025-10-21 19:16:54 +0100

    Update Theme.js

--- src/Components/Theme.js
+++ src/Components/Theme.js
@@ -30,7 +30,13 @@ export const Theme = () => {
    else 
    if(colore!=='blue' || colore!=='pink' || colore!=='green' || colore!=='red' || colore!=='purple' || colore!=='yellow' || !colore )
    {
-       dispatch(themeMode(color))
+       dispatch(themeMode([
+  "text-blue-500",
+  "border-blue-500",
+  "bg-blue-500",
+  "hover:bg-blue-300",
+  "bg-blue-300",
+  "hover:text-blue-500"]))
   setSearchParams(prev =>{ prev.set("color" , `${color[0].slice(5,(color[0].length)-4)}`)
   return prev
   });
