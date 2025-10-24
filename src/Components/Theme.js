0f4a2ddf51e2a5d0809d23c3a0cb4800b626f48a
Karemmk <100991187+Karemmk@users.noreply.github.com>
2025-10-24 20:23:19 +0100

    Update Theme.js

--- src/Components/Theme.js
+++ src/Components/Theme.js
@@ -28,7 +28,7 @@ export const Theme = () => {
   });
     }
    else 
-   if(colore!=='blue' || colore!=='pink' || colore!=='green' || colore!=='red' || colore!=='purple' || colore!=='yellow' || !colore )
+   if(colore!=='blue' || colore!=='pink' || colore!=='green' || colore!=='red' || colore!=='purple' || colore!=='yellow' )
    {
   dispatch(themeMode(color))
   setSearchParams(prev =>{ prev.set("color" , `${color[0].slice(5,(color[0].length)-4)}`)
