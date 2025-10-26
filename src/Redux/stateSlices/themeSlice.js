6dbca4b72459a5dbeebbcbbd0abfe1b9a881a4fb
Karemmk <100991187+Karemmk@users.noreply.github.com>
2025-10-26 13:39:11 +0100

    Update themeSlice.js

--- src/Redux/stateSlices/themeSlice.js
+++ src/Redux/stateSlices/themeSlice.js
@@ -1,7 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+// Get the current URL's search params
+const searchParams = new URLSearchParams(window.location.search);
+
+// Get the color value
+const colorParam = searchParams.get("color") ; // fallback if missing
+
 const initialState = {
-  value:[]
+  value:[
+    `text-${colorParam}-500`,
+      `border-${colorParam}-500`,
+      `bg-${colorParam}-500`,
+      `hover:bg-${colorParam}-300`,
+      `bg-${colorParam}-300`,
+      `hover:text-${colorParam}-500`
+  ]
 };
 
 export const themeSlice = createSlice({
