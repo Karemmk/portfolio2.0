8e758e80c0c15feb1d58fb02d663a74bc6849513
Karemmk <100991187+Karemmk@users.noreply.github.com>
2025-10-26 01:26:57 +0100

    Update Theme.js

--- src/Components/Theme.js
+++ src/Components/Theme.js
@@ -19,7 +19,7 @@ export const Theme = () => {
     dispatch(isOpen());
   };
    useEffect(() => {
-    if (!colore && !color) {
+    if (!colore && color===[]) {
       setSearchParams(prev =>{ prev.set("color" , "blue")
   return prev
          });
