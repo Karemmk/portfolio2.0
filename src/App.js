83df76c16ba04a71cafb2de470a49929891a9e06
Karemmk <100991187+Karemmk@users.noreply.github.com>
2025-10-28 00:01:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ const App = () => {
               navopen
                 ? dark
                   ? "fixed  w-2/4 bg-gray-800 text-gray-200 pt-10 p-10 z-30"
-                  : "fixed overflow-auto start-0 w-1/5 bg-white hover:w-2/5 pt-10 p-10 z-50"
+                  : "fixed start-0 w-1/5 bg-white hover:w-2/5 pt-10 p-10 z-50"
                 : dark
                 ? "hidden sm:inline-block sm:w-1/4 bg-gray-800 text-gray-200"
                 : "hidden bg-white sm:w-1/4 sm:inline-block"
