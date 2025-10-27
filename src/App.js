60a143b5c504d6951a13dc7561142d46f1ab36e6
Karemmk <100991187+Karemmk@users.noreply.github.com>
2025-10-27 09:49:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ const App = () => {
               navopen
                 ? dark
                   ? "fixed  w-2/4 bg-gray-800 text-gray-200 pt-10 p-10 z-30"
-                  : "fixed  start-0  bg-white w-2/4 pt-10 p-10 z-30"
+                  : "fixed  start-0  bg-white w-2/4 hover:w-3/5 pt-10 p-10 z-30"
                 : dark
                 ? "hidden sm:inline-block sm:w-1/4 bg-gray-800 text-gray-200"
                 : "hidden bg-white sm:w-1/4 sm:inline-block"
@@ -66,7 +66,7 @@ const App = () => {
             className={
               dark
                 ? "h-full overflow-hidden sm:w-3/4 bg-gray-900 text-gray-200 pb-20"
-                : "h-full overflow-hidden sm:w-3/4 hover:w-4/5 bg-gray-200 pb-20"
+                : "h-full overflow-hidden sm:w-3/4 sm:hover:w-4/5 bg-gray-200 pb-20"
             }
           >
           
