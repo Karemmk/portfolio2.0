8f16bd48d5c4db9196396eb30892a2caaf952cfe
Karemmk <100991187+Karemmk@users.noreply.github.com>
2025-10-28 09:55:45 +0100

    Update LeftSide.js

--- src/Components/LeftSide.js
+++ src/Components/LeftSide.js
@@ -10,11 +10,11 @@ export const LeftSide = () => {
       <div className="md:h-full grid grid-cols-1  sm:gap-40 mt-8 justify-items-center ">
         <div className="flex font-bold font-9">
           <div
-            className={`w-4 h-4 ${color}  border-t-6 border-l-6`}
+            className={`absolute ml-[5px] w-5 h-5 ${color}  border-t-8 border-l-8`}
           ></div>
           <div className="mt-2 text-lg ">KaRem</div>
           <div
-            className={`w-4 h-4 ${color} border-b-6 border-r-6 mt-5 lg:mt-12`}
+            className={`absolute mr-[5px] w-5 h-5 ${color} border-b-8 border-r-8 mt-5 lg:mt-12`}
           ></div>
         </div>
         <div>
